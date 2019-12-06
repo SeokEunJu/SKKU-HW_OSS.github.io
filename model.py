64db0ab947169f1a3f1fad4034eaf84f4116c718
JHLew <fudojhl@gmail.com>
2019-12-07 03:15:02 +0900

    added functions for loading and preprocessing procedure to feed the FAN network

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-
+import torch
 
 class Generator(nn.Module):
     def __init__(self):
@@ -56,3 +56,34 @@ class example_generator(nn.Module):
         x = self.layer_2(x)
 
         return x
+
+
+def load_FAN():
+    '''
+    input: (256, 256, 3) image with scale [0, 1]
+    output: (256, 256, 68) face landmarks
+    '''
+    from FAN.models import FAN
+    import torch.utils.model_zoo as model_zoo
+    print('========== Loading FAN model ==========')
+    model = FAN(2)
+
+    weights = model_zoo.load_url('https://www.adrianbulat.com/downloads/python-fan/2DFAN4-11f355bf06.pth.tar', map_location=lambda storage, loc: storage)
+
+    #cutoff two hourglass network
+    pretrained_dict = {k: v for k, v in weights.items() if k in model.state_dict()}
+
+    model.load_state_dict(pretrained_dict)
+    model.eval()
+
+    return model
+
+
+class upsample(nn.Module):
+    def __init__(self):
+        super(upsample, self).__init__()
+
+        self.upsample = torch.nn.UpsamplingBilinear2d([256, 256])
+
+    def forward(self, input):
+        return (self.upsample(input) + 1.) / 2

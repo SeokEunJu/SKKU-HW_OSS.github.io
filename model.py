3b22b6e1bbe64ae9522ca6579b8aa2e69b820523
JHLew <fudojhl@gmail.com>
2019-12-09 19:29:19 +0900

    removed example generator

--- model.py
+++ model.py
@@ -33,31 +33,6 @@ class ResidualBlock(nn.Module):
         return
 
 
-class example_generator(nn.Module):
-    # initial setting of the network
-    # name each part of the network
-    def __init__(self):
-        super(example_generator, self).__init__()
-        self.layer_1 = nn.Sequential(
-            nn.Conv2d(3, 3, 3, padding=1),
-            nn.ReLU()
-        )
-        self.layer_2 = nn.Sequential(
-            nn.Conv2d(3, 3, 3, padding=1),
-            nn.ReLU()
-        )
-
-
-    # how an input should go through the network
-    # ex: input x goes through layer_1, and layer_2. returns the value after that
-    def forward(self, x):
-        x = x.float()
-        x = self.layer_1(x)
-        x = self.layer_2(x)
-
-        return x
-
-
 def load_FAN():
     '''
     input: (256, 256, 3) image with scale [0, 1]

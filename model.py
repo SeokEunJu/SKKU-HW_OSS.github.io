d9b1e423c4123dec4108d2cd42f5443c92233eea
JHLew <fudojhl@gmail.com>
2019-12-13 21:19:25 +0900

    example for Network structure - used for step 3

--- model.py
+++ model.py
@@ -62,3 +62,28 @@ class upsample(nn.Module):
 
     def forward(self, input):
         return (self.upsample(input) + 1.) / 2
+
+
+class example_generator(nn.Module):
+    # initial setting of the network
+    # name each part of the network
+    def __init__(self):
+        super(example_generator, self).__init__()
+        self.layer_1 = nn.Sequential(
+            nn.Conv2d(3, 3, 3, padding=1),
+            nn.ReLU()
+        )
+        self.layer_2 = nn.Sequential(
+            nn.Conv2d(3, 3, 3, padding=1),
+            nn.ReLU()
+        )
+
+
+    # how an input should go through the network
+    # ex: input x goes through layer_1, and layer_2. returns the value after that
+    def forward(self, x):
+        x = x.float()
+        x = self.layer_1(x)
+        x = self.layer_2(x)
+
+        return x

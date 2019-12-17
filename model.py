9520d4abdd0a130f528b04ab891ace9ae97f1201
hyunmook-lim <53961996+hyunmook-lim@users.noreply.github.com>
2019-12-17 20:34:10 +0900

    Update model.py

--- model.py
+++ model.py
@@ -72,23 +72,76 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
+        self.block0 = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.LeakyReLU(negative_slope=0.2)
+        )
 
+        self.blocks = nn.Sequential(
+            nn.Conv2d(64, 64, 3, stride=2, padding=1),
+            nn.InstanceNorm2d(64, affine=True),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(64, 128, 3, stride=1, padding=1),
+            nn.InstanceNorm2d(128, affine=True),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(128, 128, 3, stride=2, padding=1),
+            nn.InstanceNorm2d(128, affine=True),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(128, 256, 3, stride=1, padding=1),
+            nn.InstanceNorm2d(256, affine=True),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(256, 256, 3, stride=2, padding=1),
+            nn.InstanceNorm2d(256, affine=True),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(256, 512, 3, stride=1, padding=1),
+            nn.InstanceNorm2d(512, affine=True),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(512, 512, 3, stride=2, padding=1),
+            nn.InstanceNorm2d(512, affine=True),
+            nn.LeakyReLU(0.2)
+        )
+
+        self.final_block = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(512, 1024, 1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(1024, 1, 1)
+        )
 
     def forward(self, x):
+        x = x.float()
+        x = x.cuda()
+        block0 = self.block0(x)
+        blocks = self.blocks(block0)
+        final_block = self.final_block(blocks)
 
-        return
-
-
+        return final_block
 # residual block - not required, but recommendable
 # identical structure of this block is repeated in the Generator
+
 class ResidualBlock(nn.Module):
     def __init__(self, n_channels):
         super(ResidualBlock, self).__init__()
-
+        self.conv1 = nn.Conv2d(n_channels, n_channels, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(n_channels)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(n_channels, n_channels, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(n_channels)
 
     def forward(self, x):
+        res = self.conv1(x)
+        res = self.bn1(res)
+        res = self.relu(res)
+        res = self.conv2(res)
+        res = self.bn2(res)
 
-        return
+        return x + res
 
 
 def load_FAN():
